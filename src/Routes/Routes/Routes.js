01f51286739a0d5b60dc9d8e5e3a03ef1b275699
R7558 <rupsanaworld1004@gmail.co>
2023-05-22 15:49:22 +0600

    added checkout

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -3,6 +3,7 @@ import Main from "../../Layout/Main";
 import Home from "../../pages/Home/Home/Home";
 import Login from "../../pages/Login/Login";
 import SignUp from "../../pages/SignUp/SignUp";
+import Checkout from "../../pages/Checkout/Checkout";
 
 const router = createBrowserRouter([
     {
@@ -21,6 +22,10 @@ const router = createBrowserRouter([
           path: '/signup', 
           element: <SignUp></SignUp>
         },
+        {
+          path: '/checkout/:id',
+          element: <Checkout></Checkout>,
+        }
       ]
     }
   ]);
